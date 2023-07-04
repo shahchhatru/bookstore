de794d2c7e7b344ac7d13bda9b699e7c4119cdd5
chhatra shah <shahkhesavchhatra@gmail.com>
2023-07-04 22:20:37 +0545

    header_completed

---
+++ components/Footer.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { footerLinks } from '@/constants';
+
+ const Footer = () => {
+  return (
+    <footer className='flex flex-col text-black-100  mt-5 border-t border-gray-100'>
+    <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+      <div className='flex flex-col justify-start items-start gap-6'>
+        <Image src='/bookstorelogo.svg' alt='logo' width={118} height={18} className='object-contain' />
+        <p className='text-base text-gray-700'>
+          BookStore 2023 <br />
+          All Rights Reserved &copy;
+        </p>
+      </div>
+      <div className='footer__links'>
+
+      </div>
+    </div>
+
+
+
+   </footer>
+  )
+}
+
+export  default Footer;
